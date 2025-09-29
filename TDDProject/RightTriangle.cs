0571c120db7e469d457373fb4a2b5458472d9c27
rakdav <rakdav@inbox.ru>
2025-09-29 16:30:05 +0200

    TDD

---
+++ TDDProject/RightTriangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProject
+{
+    public class RightTriangle
+    {
+        public double A {  get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public bool IsRight()
+        {
+            bool hA=A*A==B*B+C*C;
+            bool hB = B * B == A * A + C * C;
+            bool hC = C * C == A * A + B * B;
+            return hA||hB||hC;
+        }
+        public double Perimetr()
+        {
+            return A+B+C;
+        }
+        public double Area()
+        {
+            var (a, b, c) = (A,B,C);
+           if(A>B&&B>C)
+            {
+                c = A;
+                a = C;
+            }
+           if(B > A && B > C)
+            {
+                c = B;
+                b = C;
+            }
+            return (a * b) / 2;
+        }
+    }
+}
---
+++ TDDProject/TestTriangle.cs
@@ -0,0 +1,129 @@
+﻿namespace TDDProject
+{
+    [TestClass]
+    public class TestTriangle
+    {
+        ///<summary>
+        ///Позитивный тест
+        ///Треугольник прямоугольный
+        ///</summary>
+        [TestMethod]
+        public void isRightTest()
+        {
+            RightTriangle t=new RightTriangle();
+            t.A = 3;
+            t.B = 4;
+            t.C = 5;
+            Assert.IsTrue(t.IsRight());
+        }
+        ///<summary>
+        ///Негативный тест
+        ///Треугольник непрямоугольный
+        ///</summary>
+        [TestMethod]
+        public void isNotRightTest()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 3;
+            t.B = 4;
+            t.C = 4;
+            Assert.IsFalse(t.IsRight());
+        }
+        ///<summary>
+        ///Позитивный тест
+        ///Треугольник прямоугольный
+        ///Гипотенуза - А
+        ///</summary>
+        [TestMethod]
+        public void isRightTestA()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 10;
+            t.B = 6;
+            t.C = 8;
+            Assert.IsTrue(t.IsRight());
+        }
+        ///<summary>
+        ///Позитивный тест
+        ///Треугольник прямоугольный
+        ///Гипотенуза - B
+        ///</summary>
+        [TestMethod]
+        public void isRightTestB()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 3;
+            t.B = 5;
+            t.C = 4;
+            Assert.IsTrue(t.IsRight());
+        }
+        ///<summary>
+        ///Позитивный тест
+        ///Треугольник прямоугольный
+        ///Гипотенуза - C
+        ///</summary>
+        [TestMethod]
+        public void isRightTestC()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 3;
+            t.B = 4;
+            t.C = 5;
+            Assert.IsTrue(t.IsRight());
+        }
+        ///<summary>
+        ///Позитивный тест
+        ///Вычисление периметра
+        ///</summary>
+        [TestMethod]
+        public void PerimetrTest()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 3;
+            t.B = 4;
+            t.C = 5;
+            Assert.AreEqual(12,t.Perimetr());
+        }
+        ///<summary>
+        ///Позитивный тест
+        ///Вычисление площади
+        ///</summary>
+        [TestMethod]
+        public void AreaTest()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 3;
+            t.B = 4;
+            t.C = 5;
+            Assert.AreEqual(6, t.Area());
+        }
+        ///<summary>
+        ///Позитивный тест
+        ///Вычисление площади
+        ///Гипотенуза a
+        ///</summary>
+        [TestMethod]
+        public void AreaTestA()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 5;
+            t.B = 4;
+            t.C = 3;
+            Assert.AreEqual(6, t.Area());
+        }
+        ///<summary>
+        ///Позитивный тест
+        ///Вычисление площади
+        ///Гипотенуза b
+        ///</summary>
+        [TestMethod]
+        public void AreaTestB()
+        {
+            RightTriangle t = new RightTriangle();
+            t.A = 3;
+            t.B = 5;
+            t.C = 4;
+            Assert.AreEqual(6, t.Area());
+        }
+    }
+}
